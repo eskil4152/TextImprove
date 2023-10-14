c845a3c7da715ec79aaa0ee1a2e421145aa19a1e
eskil <eskiltb@hotmail.com>
2023-10-14 16:38:01 +0200

    Base stuff done

--- Program.cs
+++ Program.cs
@@ -4,7 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Please enter the path of the file you wish to improve: ");
+
+        bool validInput = false;
+        string filePath = "";
+
+        while (!validInput)
+        {
+            string? input = Console.ReadLine();
+            if (input != null)
+            {
+                filePath = input;
+                validInput = true;
+            }
+        }
+
+        try
+        {
+            string fileContents = File.ReadAllText(filePath);
+            Console.WriteLine("File Contents: ");
+            Console.WriteLine(fileContents);
+
+        } catch (FileNotFoundException)
+        {
+            Console.WriteLine("No file found at given path");
+        } catch (Exception ex)
+        {
+            Console.WriteLine($"An error occured: {ex}");
+        }
+        
     }
 }
 
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # TextImprove
+
+An app for reading texts, and improving readbility, grammar and spelling.
+
+## TODO:
+- [ ] Read file paths
+- [ ] Read file contents
+- [ ] Link API
+- [ ] Pass texts to API
+- [ ] Return improved text
+- [ ] Replace contents
+- [ ] Allow for docx
+- [ ] Allow for pages
